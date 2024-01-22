460f6a87f31fe52c566fb5ae4f875becc4d0000f
miok <aluvy_@naver.com>
2024-01-22 17:05:22 +0900

    Create TodoHeader.vue

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+header { display: block; text-align: center; margin: 4rem 0 2rem; }
+h1 { color: #2f3b52; font-weight: 700; }
+</style>
