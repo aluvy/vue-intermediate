e34b399db6d7e694dd55abadbc62d0facb2e6c70
miok <aluvy_@naver.com>
2024-01-22 17:05:11 +0900

    Create TodoFooter.vue

---
+++ vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    &copy; footer
+  </footer>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer { display: block; font-size: 1.2rem; text-align: center; }
+</style>
