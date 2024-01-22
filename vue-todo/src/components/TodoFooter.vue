b051aedde1c11433410bf84b1a88c27041259ff9
miok <aluvy_@naver.com>
2024-01-22 22:04:10 +0900

    1차

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,19 +1,22 @@
 <template>
   <footer>
-    &copy; footer
+    <span>&copy; footer</span>
+    <button type="button" v-on:click="clearTodo">Clear All</button>
   </footer>
 </template>
 
 <script>
 export default {
-  data: function() {
-    return {
-
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
     }
   }
 }
 </script>
 
 <style scoped>
-footer { display: block; font-size: 1.2rem; text-align: center; }
+footer { display: flex; align-items: center; justify-content: center; font-size: 1.2rem; text-align: center; }
+footer span { margin-right: auto; }
+footer button { background: #fff; border-radius: 5px; padding: 1rem 1.6rem; color: red; }
 </style>
