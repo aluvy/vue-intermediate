7daedf23c4fcde3565f35b6b648613f287eb5b44
miok <aluvy_@naver.com>
2024-01-23 17:07:06 +0900

    Update TodoHeader.vue

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -4,16 +4,6 @@
   </header>
 </template>
 
-<script>
-export default {
-  data: function() {
-    return {
-
-    }
-  }
-}
-</script>
-
 <style scoped>
 header { display: block; text-align: center; margin: 4rem 0 2rem; }
 h1 { color: #2f3b52; font-weight: 700; }
