7a1cc150ed7bf22cb39e690ec974ed6160b1de4e
miok <aluvy_@naver.com>
2024-01-23 10:43:46 +0900

    Update App.vue

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import './reset.css'
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
@@ -35,8 +36,7 @@ export default {
       this.setLocalStorage();
     },
     toggleComplete: function(a, i) {
-      a.completed = !a.completed;
-      this.todoItems[i] = a;
+      this.todoItems[i].completed = !this.todoItems[i].completed;
       this.setLocalStorage();
     },
     clearTodo: function() {
@@ -67,35 +67,5 @@ export default {
 </script>
 
 <style>
-* { box-sizing: border-box; }
-html, body { padding: 0; margin: 0; }
-html { font-size: 10px; }
-body { background: #f6f6f6; font-size: 16px; font-family: Ubuntu, Avenir, Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; color: #2f3b52; }
-p,
-fieldset,
-input,
-button { border: 0 none; padding: 0; margin: 0; background: transparent; font-size: inherit; font-family: inherit; }
-ul,
-li,
-ol { padding: 0; margin: 0; list-style: none; }
-
-label:hover,
-button:hover { cursor: pointer; }
-
-input[type='text']:focus { outline: none; }
-
-.shadow { box-shadow: 0rem 0.7rem 1rem rgba(0,0,0, 0.07); }
-.blind { position: relative;
-    z-index: -1;
-    display: inline-block;
-    overflow: hidden;
-    height: 1px;
-    width: 1px;
-    border: 0;
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    word-break: initial;
-    word-wrap: initial; }
-
 #app { width: 100%; max-width: 46rem; margin: 0 auto; }
 </style>
