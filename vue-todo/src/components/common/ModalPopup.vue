b84a7692c47eff65f7e5c29d1997fc555efac411
miok <aluvy_@naver.com>
2024-01-24 16:25:41 +0900

    Update ModalPopup.vue

--- vue-todo/src/components/common/ModalPopup.vue
+++ vue-todo/src/components/common/ModalPopup.vue
@@ -1,7 +1,7 @@
 <template>
   <Transition name="modal">
     <div v-if="show" class="modal-mask" :class="{ on: show }">
-      <div class="modal-container" ref="modalContainer">
+      <div class="modal-container" ref="modalContainer" tabindex="0" role="dialog" aria-modal="true">
 
         <div class="modal-header">
           <slot name="header">default header</slot>
@@ -15,7 +15,7 @@
           <slot name="footer"></slot>  
         </div>
 
-        <button class="modal-default-button" @click="$emit('close')">
+        <button class="modal-default-button" @click="$emit('close')" @blur="focusOn">
           <i class="fa-solid fa-xmark"></i>
         </button>
 
@@ -29,25 +29,27 @@ export default {
   props: {
     show: Boolean
   },
+  methods: {
+    focusOn () {
+      const target = this.$refs.modalContainer;
+      setTimeout(()=>{ target.focus() }, 0);
+    }
+  },
+  updated () {
+    const isOpen = this.show;
+    const target = isOpen ? this.$refs.modalContainer : this.$parent.$parent.$refs.focusBtn;
+    setTimeout(()=>{ target.focus() }, 0);
+  },
 }
 </script>
 
-<style>
-.modal-mask { position: fixed; left: 0; top: 0; display: flex; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); transition: opacity 0.3s ease; z-index: 9998; }
-.modal-container { position: relative; width: 30rem; margin: auto; padding: 2rem 3rem; background-color: #fff; border-radius: 2px; box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33); transition: all 0.3s ease; }
+<style scoped>
+.modal-mask { position: fixed; left: 0; top: 0; display: flex; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); transition: opacity .3s ease; z-index: 9998; }
+.modal-container { position: relative; width: 30rem; margin: auto; padding: 2rem 3rem; background-color: #fff; border-radius: 2px; box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33); transition: all .3s ease; }
 
 .modal-header h3 { padding-right: 3rem; color: #42b983; }
 .modal-body { margin: 2rem 0; }
 .modal-default-button { position: absolute; right: 2rem; top: 1.4rem; width: 3rem; height: 3rem; }
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
 
 .modal-enter-active { transition: all .2s ease; }
 .modal-leave-active { transition: all .3s ease; }
